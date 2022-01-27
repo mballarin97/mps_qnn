e257ee2f0c0f59a1dc49699e39cdd209df8989fe
Marco Ballarin <marco97.ballarin@gmail.com>
2022-01-27 12:19:42 +0100

    Added entanglement characterization files

---
+++ ent_char/circuits.py
@@ -0,0 +1,66 @@
+"""
+Circuit architectures not present in the qiskit package
+"""
+
+from qiskit import QuantumCircuit
+from qiskit.circuit import ParameterVector
+
+def piramidal_circuit(num_qubits, num_reps=1, piramidal=True, barrier=False):
+    """Create the piramidal circuit generalization 
+        corresponding to the circuit 12 of the paper [ADD REF].
+    
+    Parameters
+    ----------
+    num_qubits : int
+        Total number of qubits in the system
+    num_reps : int, optional
+        Number of repetitions. It is particolarly important when 
+        piramidal=False. By default 1
+    piramidal : bool, optional
+        If you schould use the piramidal architecture. If False, only
+        the first two layers are used, and repeated following the num_reps
+        parameter, by default True
+    barrier : bool, optional
+        If True, insert a barrier after each repetition. Default to False.
+
+    Returns
+    -------
+    circ : QuantumCircuit
+        The parametric quantum circuit
+    """
+    circ = QuantumCircuit(num_qubits)
+
+    # Compute the number of parameters
+    if piramidal:
+        num_params = 0
+        for ii in range(num_qubits//2):
+            for jj in range(ii, num_qubits-ii, 2):
+                num_params += 4
+    else:
+        num_params = 2*num_qubits+ 2*(num_qubits-2)
+    num_params *= num_reps
+
+    params = ParameterVector('θ', length=num_params)
+    param_idx = 0
+    for rep in range(num_reps):
+        for ii in range(num_qubits//2):
+            # Apply onlt the first two layers if piramidal=False
+            if not piramidal:
+                if ii>1:
+                    break
+            for jj in range(ii, num_qubits-ii, 2):
+                circ.ry(params[param_idx], jj)
+                param_idx += 1
+                circ.rz(params[param_idx], jj)
+                param_idx += 1
+                circ.ry(params[param_idx], jj+1)
+                param_idx += 1
+                circ.rz(params[param_idx], jj+1)
+                param_idx += 1
+                circ.cz(jj, jj+1)
+
+        if barrier:
+            circ.barrier()
+
+    return circ
+
