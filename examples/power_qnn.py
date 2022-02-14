60e73014c503c37a328eb451d1ca193361e87e36
Marco Ballarin <marco97.ballarin@gmail.com>
2022-02-14 14:55:04 +0100

    Added example folder. Here we should keep the code to launch simulations. Added requirements

---
+++ examples/power_qnn.py
@@ -0,0 +1,21 @@
+import numpy as np
+from qcircha import entanglement_scaling
+
+seed = 42
+np.random.seed(seed)
+
+# Quantum Cirucit structure
+#num_qubits = 8
+alternate = True
+
+# Choose simulation backend
+#backend = 'MPS'
+backend = 'Aer'
+
+max_num_qubits = 6 #np.arange(30, 51, 10)
+entanglement_scaling(max_num_qubits, backend = backend, alternate = alternate,
+                        max_bond_dim=1024, path='./data/ent_scaling/mps/')
+
+#main(num_qubits, backend=backend, alternate=alternate)
+#ent_vs_reps(num_qubits, alternate = alternate, backend=backend)
+#alt_comparison(num_qubits, backend=backend)
